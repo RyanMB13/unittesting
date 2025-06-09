8e7aee737f5710805cc1829a7377b268e028a2f0
GizaRoi <itsroimarc720@gmail.com>
2025-06-09 21:13:16 +0800

    added Stack class and stack object

--- test/stack.test.js
+++ test/stack.test.js
@@ -1,4 +1,18 @@
+class Stack{
+    constructor(){
+        this.top = -1
+        this.items = []
+    }
+}
+
 describe('My Stack', () => {
+    
+    let stack
+
+    beforeEach(() => {
+        stack = new Stack;
+    })
+    
     it('is empty when created', () => {
         expect(stack.top).toBe(-1);
         expect(stack.items).toEqual([]);
