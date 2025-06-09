dc8efb8d25eef6e8a8be033c18959ff5dc23f656
RyanMB13 <ryanbautista1624@yahoo.com>
2025-06-09 19:06:57 +0800

    Dev A failing test

--- test/stack.test.js
+++ test/stack.test.js
@@ -1,11 +1,4 @@
 describe('My Stack', () => {
-
-    let stack;
-
-    beforeEach(() => {
-        stack = new Stack();
-    });
-
     it('is empty when created', () => {
         expect(stack.top).toBe(-1);
         expect(stack.items).toEqual([]);
